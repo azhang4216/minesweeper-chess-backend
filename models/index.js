a026a84e500192d08f465d5658bbbc3b20ffef05
Angela Zhang <angela.zhang@dali.dartmouth.edu>
2025-06-01 00:09:09 -0700

    add: controllers and models index files

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const ActivePlayer = require("./activePlayer");
+const User = require("./user");
+const VerificationToken = require("./verificationToken");
+
+module.exports = {
+    ActivePlayer,
+    User,
+    VerificationToken
+};
